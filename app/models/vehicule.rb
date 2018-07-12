3416b6a31a4c793e941f56b4e7928f5373073bb5
yassine elabbassi <yassine@elabbassi.org>
2018-07-12 16:51:10 +0100

    vehicule validation

--- app/models/vehicule.rb
+++ app/models/vehicule.rb
@@ -1,3 +1,8 @@
 class Vehicule < ApplicationRecord
+  validates_inclusion_of :carburant, :in => %w(D E)
+  validates_inclusion_of :cv, :in => 1..18
+  validates_inclusion_of :cylindre, :in => 1..12
+  validates_inclusion_of :place, :in => 0..99
+
   mount_uploader :scan, ScanUploader
 end
